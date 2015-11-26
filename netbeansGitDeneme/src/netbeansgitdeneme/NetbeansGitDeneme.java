b675d6504f7b8d70bb6345fc73d5d3011450ee5c
fcagil <mehmetfcagil@gmail.com>
2015-11-26 20:08:05 +0200

    deneme 1,2,3

--- netbeansGitDeneme/src/netbeansgitdeneme/NetbeansGitDeneme.java
+++ netbeansGitDeneme/src/netbeansgitdeneme/NetbeansGitDeneme.java
@@ -14,6 +14,7 @@ public class NetbeansGitDeneme {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("AAAAA");
     }
     
 }
