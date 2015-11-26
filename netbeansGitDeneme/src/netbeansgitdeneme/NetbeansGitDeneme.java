ffcb79ec14b0cdcdecbe5304b429000ea9e79007
fcagil <mehmetfcagil@gmail.com>
2015-11-26 20:05:59 +0200

    deneme 1,2,3

---
+++ netbeansGitDeneme/src/netbeansgitdeneme/NetbeansGitDeneme.java
@@ -0,0 +1,19 @@
+/*
+ * NetBeans Github deneme :(
+ */
+package netbeansgitdeneme;
+
+/**
+ *
+ * @author user
+ */
+public class NetbeansGitDeneme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
